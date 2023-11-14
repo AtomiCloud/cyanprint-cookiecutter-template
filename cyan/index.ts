a63d6413740e61550e0a99a76b9253e5f3666d86
kirinnee <kirinnee97@gmail.com>
2023-11-14 20:02:15 +0800

    feat: initial commit

---
+++ cyan/index.ts
@@ -0,0 +1,53 @@
+import {QuestionType, StartTemplateWithLambda} from "@atomicloud/cyan-sdk";
+import simpleGit from "simple-git";
+import * as fs from "fs";
+import {v4} from "uuid";
+
+const git = simpleGit();
+StartTemplateWithLambda(async (i, d) => {
+
+    const url = await i.text("CookieCutter Git URL");
+
+
+    const tmpPath = `/tmp/cookiecutter/${v4()}`
+    fs.mkdirSync(tmpPath, {recursive: true});
+
+    git.clone(url, tmpPath);
+
+    const cookieCutterJson = fs.readFileSync(`${tmpPath}/cookiecutter.json`, 'utf-8');
+
+    const cookieCutter = JSON.parse(cookieCutterJson);
+
+    const results: Record<string, string> = {}
+
+    for (const [key, value] of Object.entries(cookieCutter)) {
+        if (typeof value === "string") {
+            if (["1", "true", "t", "yes", "y", "on", "0", "false", "f", "no", "n", "off"].includes(value.toLowerCase())) {
+                results[key] = (await i.confirm({
+                    type: QuestionType.Confirm,
+                    message: key,
+                })) ? "true" : "false";
+            } else {
+                results[key] = await i.text({
+                    type: QuestionType.Text,
+                    message: key,
+                    default: value,
+                });
+            }
+        }
+    }
+
+    return {
+        processors: [
+            {
+                name: "cyan/cookiecutter",
+                files: [],
+                config: {
+                    template: url,
+                    config: results,
+                },
+            },
+        ],
+        plugins: [],
+    }
+});
