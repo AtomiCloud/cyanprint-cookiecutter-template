30e004a53dca398a4f8bc03a1ff31f8c6382da81
kirinnee <kirinnee97@gmail.com>
2023-11-14 20:16:23 +0800

    fix: multiple choice

--- cyan/index.ts
+++ cyan/index.ts
@@ -1,53 +1,75 @@
-import {QuestionType, StartTemplateWithLambda} from "@atomicloud/cyan-sdk";
+import { QuestionType, StartTemplateWithLambda } from "@atomicloud/cyan-sdk";
 import simpleGit from "simple-git";
 import * as fs from "fs";
-import {v4} from "uuid";
+import { v4 } from "uuid";
 
 const git = simpleGit();
 StartTemplateWithLambda(async (i, d) => {
+  const url = await i.text("CookieCutter Git URL");
 
-    const url = await i.text("CookieCutter Git URL");
+  const tmpPath = `/tmp/cookiecutter/${v4()}`;
+  fs.mkdirSync(tmpPath, { recursive: true });
 
+  git.clone(url, tmpPath);
 
-    const tmpPath = `/tmp/cookiecutter/${v4()}`
-    fs.mkdirSync(tmpPath, {recursive: true});
+  const cookieCutterJson = fs.readFileSync(
+    `${tmpPath}/cookiecutter.json`,
+    "utf-8",
+  );
 
-    git.clone(url, tmpPath);
+  const cookieCutter = JSON.parse(cookieCutterJson);
 
-    const cookieCutterJson = fs.readFileSync(`${tmpPath}/cookiecutter.json`, 'utf-8');
+  const results: Record<string, string> = {};
 
-    const cookieCutter = JSON.parse(cookieCutterJson);
-
-    const results: Record<string, string> = {}
-
-    for (const [key, value] of Object.entries(cookieCutter)) {
-        if (typeof value === "string") {
-            if (["1", "true", "t", "yes", "y", "on", "0", "false", "f", "no", "n", "off"].includes(value.toLowerCase())) {
-                results[key] = (await i.confirm({
-                    type: QuestionType.Confirm,
-                    message: key,
-                })) ? "true" : "false";
-            } else {
-                results[key] = await i.text({
-                    type: QuestionType.Text,
-                    message: key,
-                    default: value,
-                });
-            }
-        }
+  for (const [key, value] of Object.entries(cookieCutter)) {
+    if (typeof value === "string") {
+      if (
+        [
+          "1",
+          "true",
+          "t",
+          "yes",
+          "y",
+          "on",
+          "0",
+          "false",
+          "f",
+          "no",
+          "n",
+          "off",
+        ].includes(value.toLowerCase())
+      ) {
+        results[key] = (await i.confirm({
+          type: QuestionType.Confirm,
+          message: key,
+        }))
+          ? "true"
+          : "false";
+      } else {
+        results[key] = await i.text({
+          type: QuestionType.Text,
+          message: key,
+          default: value,
+        });
+      }
+    } else if (Array.isArray(value)) {
+      results[key] = await i.select(key, value);
+    } else {
+      throw new Error(`Unknown type ${typeof value}`);
     }
+  }
 
-    return {
-        processors: [
-            {
-                name: "cyan/cookiecutter",
-                files: [],
-                config: {
-                    template: url,
-                    config: results,
-                },
-            },
-        ],
-        plugins: [],
-    }
+  return {
+    processors: [
+      {
+        name: "cyan/cookiecutter",
+        files: [],
+        config: {
+          template: url,
+          config: results,
+        },
+      },
+    ],
+    plugins: [],
+  };
 });
